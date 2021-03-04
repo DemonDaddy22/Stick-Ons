01fd4779ff161cfd31dfd3663721ef497b671359
Rohan Gupta <guptarohan369@gmail.com>
2021-03-04 18:49:49 +0530

    Add forms to modal and add new list creation logic

--- src/index.js
+++ src/index.js
@@ -2,14 +2,57 @@ const addListButton = document.querySelector('#add-list');
 const dashboard = document.querySelector('.dashboard-wrapper');
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal-content');
+const newListModalContent = document.querySelector('.modal-content-new-list');
+const newCardModalContent = document.querySelector('.modal-content-new-card');
+const modalTitle = document.querySelector('.modal-title');
+const modalBody = document.querySelector('.modal-body');
+const newListSubmit = document.querySelector('#new-list-submit');
+const newCardSubmit = document.querySelector('#new-card-submit');
 const closeModalBtn = document.querySelector('#modal-close');
+const inputs = document.querySelectorAll('input[type="text"]');
+const modalError = document.querySelector('.modal-error');
 
-const openModal = () => {
+const MODAL_CONTENT = Object.freeze({
+    NEW_LIST: {
+        ELEMENT: newListModalContent,
+        ID: 'modal-content-new-list'
+    },
+    NEW_CARD: {
+        ELEMENT: newCardModalContent,
+        ID: 'modal-content-new-card'
+    }
+});
+
+const INPUTS = {
+    'list-title': '',
+    'card-title': '',
+    'card-description': ''
+};
+
+const clearInputs = () => Object.keys(INPUTS).forEach(input => INPUTS[input] = '');
+
+const openModal = modalContentElement => {
     modal.classList.remove('display-none');
-    modalContent.classList.add('animate-modal');
+    if (modalContentElement) {
+        modalContentElement.ELEMENT.classList.add('animate-modal');
+        modalContentElement.ELEMENT.classList.remove('display-none');
+        modal.dataset.target = modalContentElement.ID;
+    }
 }
 
-const closeModal = () => modal.classList.add('display-none');
+const closeModal = () => {
+    modal.classList.add('display-none');
+    const modalContentElements = Object.keys(MODAL_CONTENT).filter(entry => MODAL_CONTENT[entry].ID === modal.dataset.target);
+
+    let modalContentElement;
+    if (modalContentElements.length) modalContentElement = MODAL_CONTENT[modalContentElements[0]].ELEMENT;
+
+    if (modalContentElement) {
+        modalContentElement.classList.add('display-none');
+    }
+
+    clearInputs();
+}
 
 const createElementWithClasses = (classes, element = 'div') => {
     const newElement = document.createElement(element);
@@ -18,22 +61,36 @@ const createElementWithClasses = (classes, element = 'div') => {
 }
 
 const addNewList = () => {
-    openModal();
-    // const newList = createElementWithClasses(['column-cards']);
-    // const columnTitleWrapper = createElementWithClasses(['column-title-wrapper']);
-    // const columnTitle = createElementWithClasses(['column-title', 'text-light']);
-    // const columnBody = createElementWithClasses(['column-body']);
+    if (!INPUTS['list-title']) {
+        modalError.classList.remove('display-none');
+        return;
+    }
 
-    // columnTitle.textContent = 'Testing';
+    const newList = createElementWithClasses(['column-cards']);
+    const columnTitleWrapper = createElementWithClasses(['column-title-wrapper']);
+    const columnTitle = createElementWithClasses(['column-title', 'text-light']);
+    const columnBody = createElementWithClasses(['column-body']);
 
-    // columnTitleWrapper.appendChild(columnTitle);
-    // newList.appendChild(columnTitleWrapper);
-    // newList.appendChild(columnBody);
+    columnTitle.textContent = INPUTS['list-title'];
 
-    // dashboard.appendChild(newList);
+    columnTitleWrapper.appendChild(columnTitle);
+    newList.appendChild(columnTitleWrapper);
+    newList.appendChild(columnBody);
+
+    dashboard.appendChild(newList);
+
+    clearInputs();
+    closeModal();
 }
 
-addListButton.addEventListener('click', addNewList);
+const handleInputChange = e => {
+    const { name, value } = e.target;
+    INPUTS[name] = value;
+}
+
+addListButton.addEventListener('click', () => openModal(MODAL_CONTENT.NEW_LIST));
+
+newListSubmit.addEventListener('click', addNewList);
 
 modal.addEventListener('click', e => {
     const { left, right, top, bottom } = modalContent.getBoundingClientRect();
@@ -44,3 +101,5 @@ modal.addEventListener('click', e => {
 });
 
 closeModalBtn.addEventListener('click', closeModal);
+
+inputs.forEach(input => input.addEventListener('change', handleInputChange));
