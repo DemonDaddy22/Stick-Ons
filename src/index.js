d6e093fa207d4f3ba57753d644bed61fda85206f
Rohan Gupta <guptarohan369@gmail.com>
2021-03-04 16:06:38 +0530

    Add modal box

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 const addListButton = document.querySelector('#add-list');
 const dashboard = document.querySelector('.dashboard-wrapper');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const closeModalBtn = document.querySelector('#modal-close');
 
-const addNewList = () => {
-    const newList = createElementWithClasses(['column-cards']);
-    const columnTitleWrapper = createElementWithClasses(['column-title-wrapper']);
-    const columnTitle = createElementWithClasses(['column-title', 'text-light']);
-    const columnBody = createElementWithClasses(['column-body']);
-
-    columnTitle.textContent = 'Testing';
-
-    columnTitleWrapper.appendChild(columnTitle);
-    newList.appendChild(columnTitleWrapper);
-    newList.appendChild(columnBody);
-    
-    dashboard.appendChild(newList);
+const openModal = () => {
+    modal.classList.remove('display-none');
+    modalContent.classList.add('animate-modal');
 }
+
+const closeModal = () => modal.classList.add('display-none');
 
 const createElementWithClasses = (classes, element = 'div') => {
     const newElement = document.createElement(element);
@@ -22,4 +17,30 @@ const createElementWithClasses = (classes, element = 'div') => {
     return newElement;
 }
 
+const addNewList = () => {
+    openModal();
+    // const newList = createElementWithClasses(['column-cards']);
+    // const columnTitleWrapper = createElementWithClasses(['column-title-wrapper']);
+    // const columnTitle = createElementWithClasses(['column-title', 'text-light']);
+    // const columnBody = createElementWithClasses(['column-body']);
+
+    // columnTitle.textContent = 'Testing';
+
+    // columnTitleWrapper.appendChild(columnTitle);
+    // newList.appendChild(columnTitleWrapper);
+    // newList.appendChild(columnBody);
+
+    // dashboard.appendChild(newList);
+}
+
 addListButton.addEventListener('click', addNewList);
+
+modal.addEventListener('click', e => {
+    const { left, right, top, bottom } = modalContent.getBoundingClientRect();
+    const x = e.clientX, y = e.clientY;
+
+    if ((x >= left && x <= right) && (y >= top && y <= bottom)) return;
+    closeModal();
+});
+
+closeModalBtn.addEventListener('click', closeModal);
